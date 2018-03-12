9cc25afc0eabda30a0b088dd826f56b6e6138488
ceasaro <ceesvw@gmail.com>
2018-03-12 08:45:30 +0100

    scrape website for list of animals.

--- words/word.py
+++ words/word.py
@@ -5,7 +5,7 @@ import re
 import os
 
 
-def search(mask, has_chars=None, has_word=None):
+def search(mask, has_chars=None, has_word=None, regex=False):
     matching_words = []
     match_letter_count = '.' in mask
     mask_chars = len(mask)
@@ -18,12 +18,13 @@ def search(mask, has_chars=None, has_word=None):
         for word in open(file_with_words):
             word = word.rstrip('\n')
             matches = True
-
             for char in has_chars or []:
                 matches &= char in word
             if has_word:
                 matches &= has_word in word
             if matches:
+                if regex and re.match(mask, word):
+                    matching_words.append(word)
                 if match_letter_count and len(word) == mask_chars and re.match(mask, word):
                     matching_words.append(word)
                 elif not match_letter_count and mask in word:
@@ -37,6 +38,7 @@ def get_arg_parser():
     parser.add_argument('mask', help="The mask to search words with. (eg. 'wor..' results in 'words', 'worth', etc. ")
     parser.add_argument('-c', '--chars', help="word must include these characters")
     parser.add_argument('-w', '--word', help="word must include this word")
+    parser.add_argument('-r', '--regex', help="word must match the specified regular expression", action='store_true',)
     return parser
 
 
@@ -48,10 +50,11 @@ def main(prog_args):
                                          ', with {}'.format(args.chars) if args.chars else '',
                                          ', contains {}'.format(args.word) if args.word else ''
                                          ))
-    matches = search(mask, args.chars, args.word)
+    matches = search(mask, args.chars, args.word, args.regex)
     print ('found words:')
     for w in matches:
         print(w)
+
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv))
