0f11004dfd58145693163dc3fd1270342b936836
ceasaro <ceesvw@gmail.com>
2019-12-26 14:17:10 +0100

    refactor search code.

--- words/word.py
+++ words/word.py
@@ -7,8 +7,6 @@ import os
 
 def search(mask, has_chars=None, has_word=None, regex=False, ij_as_y=False):
     matching_words = []
-    match_letter_count = '.' in mask
-    mask_chars = len(mask)
     local_path = os.path.dirname(os.path.realpath(__file__))
     files = [
         os.path.join(local_path, 'opentaal-210G-woordenlijsten/OpenTaal-210G-basis-gekeurd.txt'),
@@ -17,21 +15,30 @@ def search(mask, has_chars=None, has_word=None, regex=False, ij_as_y=False):
     for file_with_words in files:
         for word in open(file_with_words):
             original_word = word.rstrip('\n')
-            word = original_word.replace('ij', 'y') if ij_as_y else original_word
-            matches = True
-            for char in has_chars or []:
-                matches &= char in word
-            if has_word:
-                matches &= has_word in word
-            if matches:
-                if regex and re.match(mask, word):
-                    matching_words.append(original_word)
-                if match_letter_count and len(word) == mask_chars and re.match(mask, word):
-                    matching_words.append(original_word)
-                elif not match_letter_count and mask in word:
-                    matching_words.append(original_word)
+            if word_matches(original_word, has_chars, has_word, ij_as_y, mask, regex):
+                matching_words.append(original_word)
 
     return matching_words
+
+
+def word_matches(word, has_chars, has_word, ij_as_y, mask, regex):
+    match_letter_count = '.' in mask
+    mask_chars = len(mask)
+    if ij_as_y:
+        word = word.replace('ij', 'y')
+    matches = True
+    for char in has_chars or []:
+        matches &= char in word
+    if has_word:
+        matches &= has_word in word
+    if matches:
+        if regex and re.match(mask, word):
+            return True
+        if match_letter_count and len(word) == mask_chars and re.match(mask, word):
+            return True
+        elif not match_letter_count and mask in word:
+            return True
+    return False
 
 
 def get_arg_parser():
